7d5a09ce251d001342bf79899724461918e61aae
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-18 17:28:51 -0500

    21.1

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import { StoreProvider } from './utils/GlobalState';
 import OrderHistory from './pages/OrderHistory';
 
 const httpLink = createHttpLink({
@@ -37,18 +38,20 @@ const client = new ApolloClient({
 
 function App() {
 	return (
-		<ApolloProvider client={client}>
+		<ApolloProvider client={ client }>
 			<Router>
 				<div>
-					<Nav />
-					<Switch>
-						<Route exact path="/" component={Home} />
-						<Route exact path="/login" component={Login} />
-						<Route exact path="/signup" component={Signup} />
-						<Route exact path="/orderHistory" component={OrderHistory} />
-						<Route exact path="/products/:id" component={Detail} />
-						<Route component={NoMatch} />
-					</Switch>
+					<StoreProvider>
+						<Nav />
+						<Switch>
+							<Route exact path="/" component={ Home } />
+							<Route exact path="/login" component={ Login } />
+							<Route exact path="/signup" component={ Signup } />
+							<Route exact path="/orderHistory" component={ OrderHistory } />
+							<Route exact path="/products/:id" component={ Detail } />
+							<Route component={ NoMatch } />
+						</Switch>
+					</StoreProvider>
 				</div>
 			</Router>
 		</ApolloProvider>
--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -1,1 +1,27 @@
-// GlobalState
+import React, { createContext, useContext } from 'react';
+import { useProductReducer } from './reducers';
+
+
+const StoreContext = createContext();
+const { Provider } = StoreContext;
+
+
+const StoreProvider = ({ value = [], ...props }) => {
+    const [state, dispatch] = useProductReducer({
+        products: [],
+        categories: [],
+        currentCategory: ''
+    });
+
+    // user this to confirm it works
+    console.log(state);
+    return <Provider value={ [state, dispatch] } { ...props } />;
+};
+
+const useStoreContext = () => {
+    return useContext(StoreContext);
+};
+
+
+// exporting... ... ...
+export { StoreProvider, useStoreContext };
