727b97df9acc07bf36ac22fc9bb7d30c2f9471c6
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-18 16:01:01 -0500

    created reducer actions function beased on action type; created tests for those actions

--- client/src/__tests__/reducers.test.js
+++ client/src/__tests__/reducers.test.js
@@ -1,1 +1,48 @@
-// tests
+import { reducer } from '../utils/reducers';
+
+// import our actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+
+// create a sample of what our global state will look like
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+};
+
+// test UPDATED_PRODUCTS
+test('UPDATED_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products: [{}, {}]
+    });
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
+
+// test UPDATED_CATEGORIES
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
+
+// test UPDATED_CURRENT_CATEGORIES
+test('UPDATE_CURRENT_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CURRENT_CATEGORY,
+        currentCategory: '2'
+    });
+
+    expect(newState.currentCategory).toBe('2');
+    expect(initialState.currentCategory).toBe('1');
+});
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,1 +1,41 @@
-// reducers
+import { useReducer } from 'react';
+
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from './actions';
+
+export const reducer = (state, action) => {
+    switch (action.type) {
+        // if action type value is the value of `UPDATE_PRODUCTS`, return a new state object with an updated products array
+        case UPDATE_PRODUCTS:
+            return {
+                ...state,
+                products: [...action.products],
+            };
+
+        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+        case UPDATE_CATEGORIES:
+            return {
+                ...state,
+                categories: [...action.categories]
+            };
+
+        // if action type value of the value of `UPEDATE_CURRENT_CATEGORY`, return a new state object with an updated current category array
+        case UPDATE_CURRENT_CATEGORY:
+            return {
+                ...state,
+                currentCategory: action.currentCategory
+            };
+
+        // if it's none of these actions, do not update state at all and keep things the same!
+        default:
+            return state;
+    }
+};
+
+// exporting... ... ...
+export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+};
