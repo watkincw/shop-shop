df75f8ce783f02502a54153d2a2e952cab904399
Colton Watkins <83984833+ThManHimself@users.noreply.github.com>
2021-10-18 17:11:27 -0500

    spacing

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -6,45 +6,45 @@ import { QUERY_PRODUCTS } from '../utils/queries';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const { id } = useParams();
+	const { id } = useParams();
 
-  const [currentProduct, setCurrentProduct] = useState({});
+	const [currentProduct, setCurrentProduct] = useState({});
 
-  const { loading, data } = useQuery(QUERY_PRODUCTS);
+	const { loading, data } = useQuery(QUERY_PRODUCTS);
 
-  const products = data?.products || [];
+	const products = data?.products || [];
 
-  useEffect(() => {
-    if (products.length) {
-      setCurrentProduct(products.find((product) => product._id === id));
-    }
-  }, [products, id]);
+	useEffect(() => {
+		if (products.length) {
+			setCurrentProduct(products.find((product) => product._id === id));
+		}
+	}, [products, id]);
 
-  return (
-    <>
-      {currentProduct ? (
-        <div className="container my-1">
-          <Link to="/">← Back to Products</Link>
+	return (
+		<>
+			{currentProduct ? (
+				<div className="container my-1">
+					<Link to="/">← Back to Products</Link>
 
-          <h2>{currentProduct.name}</h2>
+					<h2>{currentProduct.name}</h2>
 
-          <p>{currentProduct.description}</p>
+					<p>{currentProduct.description}</p>
 
-          <p>
-            <strong>Price:</strong>${currentProduct.price}{' '}
-            <button>Add to Cart</button>
-            <button>Remove from Cart</button>
-          </p>
+					<p>
+						<strong>Price:</strong>${currentProduct.price}{' '}
+						<button>Add to Cart</button>
+						<button>Remove from Cart</button>
+					</p>
 
-          <img
-            src={`/images/${currentProduct.image}`}
-            alt={currentProduct.name}
-          />
-        </div>
-      ) : null}
-      {loading ? <img src={spinner} alt="loading" /> : null}
-    </>
-  );
+					<img
+						src={`/images/${currentProduct.image}`}
+						alt={currentProduct.name}
+					/>
+				</div>
+			) : null}
+			{loading ? <img src={spinner} alt="loading" /> : null}
+		</>
+	);
 }
 
 export default Detail;
